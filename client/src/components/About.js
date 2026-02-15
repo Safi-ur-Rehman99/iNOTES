593682ce8bd43cd390e95245799b9a94e81a5f5a
Safi <syedsafi3414@gmail.com>
2026-02-15 17:33:06 +0500

    completed  backend api, initialised clien tside with routers

---
+++ client/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="container my-3">
+      <h1>About iNOTES</h1>
+      <p>iNOTES is a cloud-based notes application to store your notes securely.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container my-3">
+      <h1>Welcome to iNOTES</h1>
+      <p>Your notes on the cloud</p>
+    </div>
+  )
+}
+
+export default Home
